9992b03b5c14a3db4de2d11f7c39f9f95d497162
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2022-10-28 12:54:43 +0600

    fix detekt

--- permissions/src/androidMain/kotlin/dev/icerock/moko/permissions/PermissionsControllerImpl.kt
+++ permissions/src/androidMain/kotlin/dev/icerock/moko/permissions/PermissionsControllerImpl.kt
@@ -58,7 +58,9 @@ class PermissionsControllerImpl(
     }
 
     override fun isPermissionGranted(permission: Permission): Boolean {
-        if (permission == Permission.REMOTE_NOTIFICATION && Build.VERSION.SDK_INT in Build.VERSION_CODES.KITKAT until Build.VERSION_CODES.TIRAMISU) {
+        if (permission == Permission.REMOTE_NOTIFICATION &&
+            Build.VERSION.SDK_INT in VERSIONS_WITHOUT_NOTIFICATION_PERMISSION
+        ) {
             return NotificationManagerCompat.from(applicationContext).areNotificationsEnabled()
         }
         return permission.toPlatformPermission().all {
@@ -69,7 +71,9 @@ class PermissionsControllerImpl(
 
     @Suppress("ReturnCount")
     override suspend fun getPermissionState(permission: Permission): PermissionState {
-        if (permission == Permission.REMOTE_NOTIFICATION && Build.VERSION.SDK_INT in Build.VERSION_CODES.KITKAT until Build.VERSION_CODES.TIRAMISU) {
+        if (permission == Permission.REMOTE_NOTIFICATION &&
+            Build.VERSION.SDK_INT in VERSIONS_WITHOUT_NOTIFICATION_PERMISSION
+        ) {
             val isNotificationsEnabled = NotificationManagerCompat.from(applicationContext)
                 .areNotificationsEnabled()
             return if (isNotificationsEnabled) {
@@ -140,6 +144,7 @@ class PermissionsControllerImpl(
                     emptyList()
                 }
             }
+
             Permission.RECORD_AUDIO -> listOf(Manifest.permission.RECORD_AUDIO)
             Permission.BLUETOOTH_LE -> allBluetoothPermissions()
             Permission.BLUETOOTH_SCAN -> bluetoothScanCompat()
@@ -186,4 +191,9 @@ class PermissionsControllerImpl(
         } else {
             listOf(Manifest.permission.BLUETOOTH)
         }
+
+    private companion object {
+        val VERSIONS_WITHOUT_NOTIFICATION_PERMISSION =
+            Build.VERSION_CODES.KITKAT until Build.VERSION_CODES.TIRAMISU
+    }
 }
