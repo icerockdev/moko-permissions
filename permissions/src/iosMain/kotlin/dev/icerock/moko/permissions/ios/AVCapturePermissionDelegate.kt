9db4653420dad5ffdf3dec5eea27589db941bf92
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2023-04-10 12:54:26 +0600

    #63 support of limited iOS notifications permissions

--- permissions/src/iosMain/kotlin/dev/icerock/moko/permissions/ios/AVCapturePermissionDelegate.kt
+++ permissions/src/iosMain/kotlin/dev/icerock/moko/permissions/ios/AVCapturePermissionDelegate.kt
@@ -12,6 +12,7 @@ import platform.AVFoundation.AVAuthorizationStatus
 import platform.AVFoundation.AVAuthorizationStatusAuthorized
 import platform.AVFoundation.AVAuthorizationStatusDenied
 import platform.AVFoundation.AVAuthorizationStatusNotDetermined
+import platform.AVFoundation.AVAuthorizationStatusRestricted
 import platform.AVFoundation.AVCaptureDevice
 import platform.AVFoundation.AVMediaType
 import platform.AVFoundation.authorizationStatusForMediaType
@@ -34,6 +35,7 @@ internal class AVCapturePermissionDelegate(
                 if (isGranted) return
                 else throw DeniedAlwaysException(permission)
             }
+
             AVAuthorizationStatusDenied -> throw DeniedAlwaysException(permission)
             else -> error("unknown authorization status $status")
         }
@@ -44,11 +46,12 @@ internal class AVCapturePermissionDelegate(
     }
 
     override suspend fun getPermissionState(): PermissionState {
-        val status = currentAuthorizationStatus()
+        val status: AVAuthorizationStatus = currentAuthorizationStatus()
         return when (status) {
             AVAuthorizationStatusAuthorized -> PermissionState.Granted
             AVAuthorizationStatusNotDetermined -> PermissionState.NotDetermined
             AVAuthorizationStatusDenied -> PermissionState.DeniedAlways
+            AVAuthorizationStatusRestricted -> PermissionState.Granted
             else -> error("unknown authorization status $status")
         }
     }
