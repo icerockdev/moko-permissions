6a930baaddc6ffd429a37415d48655fa23351309
Adrian Witaszak <adrianwitaszak@gmail.com>
2023-06-25 10:17:13 +0100

    Added copyright

--- permissions/src/iosMain/kotlin/dev/icerock/moko/permissions/ios/MotionPermissionDelegate.kt
+++ permissions/src/iosMain/kotlin/dev/icerock/moko/permissions/ios/MotionPermissionDelegate.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2022 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 package dev.icerock.moko.permissions.ios
 
 import dev.icerock.moko.permissions.PermissionState
