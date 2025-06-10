de9c08171d145af42f6de5cf3c16419045761153
tet <tet>
2025-06-10 21:32:36 +0200

    Fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,32 @@ val mokoVersion = libs.versions.mokoPermissionsVersion.get()
 allprojects {
     group = "dev.icerock.moko"
     version = mokoVersion
+    
+    // Configure Java toolchain to use Java 17
+    plugins.withId("org.jetbrains.kotlin.multiplatform") {
+        extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
+            jvmToolchain(17)
+        }
+    }
+    
+    // For Android projects
+    plugins.withId("com.android.application") {
+        extensions.configure<com.android.build.gradle.BaseExtension> {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+    
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension> {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
 }
 
 tasks.register("clean", Delete::class).configure {
