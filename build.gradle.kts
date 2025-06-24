163899d5ebde62e757e6fcb9972b2ca33bd7f777
alobynya <alobynya@icerockdev.com>
2025-06-24 12:25:00 +0700

    Comments fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
         mavenCentral()
         google()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     dependencies {
@@ -25,32 +26,6 @@ val mokoVersion = libs.versions.mokoPermissionsVersion.get()
 allprojects {
     group = "dev.icerock.moko"
     version = mokoVersion
-    
-    // Configure Java toolchain to use Java 17
-    plugins.withId("org.jetbrains.kotlin.multiplatform") {
-        extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
-            jvmToolchain(17)
-        }
-    }
-    
-    // For Android projects
-    plugins.withId("com.android.application") {
-        extensions.configure<com.android.build.gradle.BaseExtension> {
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-        }
-    }
-    
-    plugins.withId("com.android.library") {
-        extensions.configure<com.android.build.gradle.LibraryExtension> {
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-        }
-    }
 }
 
 tasks.register("clean", Delete::class).configure {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
         google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
